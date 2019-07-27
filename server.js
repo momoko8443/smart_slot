072bf733e21f06233233ae59253ee416a0a13e2f
Jake.Zheng <Huibin.zheng@hpe.com>
2019-07-27 09:07:18 +0800

    up

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const slotController = require('./libs/SlotController')
 
 const slot1 = slotController.registryPin(17,'out');
 const slot2 = slotController.registryPin(27,'out');
+const sensor1 = slotController.registryPin(22,'in');
 
 slot1.set();
 slot2.set();
+sensor1.on('change',(value)=>{
+    console.log('sensor:'+value);
+})
 
 app.put('/gpio/:id', (req, res) => {
     res.send('Hello World!')

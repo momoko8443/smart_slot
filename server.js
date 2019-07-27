23e1b14077e7e0655f4b7122aafda30bd65ed37e
Jake.Zheng <Huibin.zheng@hpe.com>
2019-07-27 09:24:11 +0800

    up

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ const express = require('express')
 const app = express()
 const slotController = require('./libs/SlotController')
 
+let isDry = false;
 const slot1 = slotController.registryPin(17,'out');
 const slot2 = slotController.registryPin(27,'out');
 const sensor1 = slotController.registryPin(22,'in',(val)=>{
-    console.log('sensor init value:' + val);
+    console.log('sensor init value:' + sensor1.value);
+    isDry = sensor1.value ? true : false;
 });
 
-//slot1.set();
-//slot2.set();
+slot1.reset();
+slot2.reset();
 
-let isDry = false;
+
 sensor1.on('change',(value)=>{
     //0: wet, 1:dry
     console.log('sensor:'+value);
+    isDry = sensor1.value ? true : false;
 })
 
 app.put('/gpio/:id', (req, res) => {

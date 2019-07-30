d951c4427f6ae49945f1b217994937116a384464
Jake.Zheng <Huibin.zheng@hpe.com>
2019-07-30 17:54:45 +0800

    update

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const slotController = require('./libs/SlotController')
+
+app.use(bodyParser.json()); 
+app.use(express.static('public'))
 
 let isDry = false;
 const slot1 = slotController.registryPin(17,'out');
@@ -18,10 +22,20 @@ sensor1.on('change',(value)=>{
     //0: wet, 1:dry
     console.log('sensor:'+value);
     isDry = sensor1.value ? true : false;
-})
+});
 
-app.put('/gpio/:id', (req, res) => {
-    res.send('Hello World!')
+app.get('/api/water',(req, res)=>{
+    res.send({'dry': isDry});
+});
+app.post('/api/water', (req, res) => {
+    const time = Number.parseInt(req.body.seconds) * 1000;
+    slot1.set();
+    slot2.set();
+    setTimeout(()=>{
+        slot1.reset();
+        slot2.reset();
+        res.sendStatus(200);
+    },time)
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
